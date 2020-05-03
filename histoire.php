2aa7df196729b9f41752c0411219a4e7d42927d0
cydaw6 <cydaw6@gmail.com>
2020-05-04 00:29:55 +0200

    Update histoire.php
    mise en place de la time line

--- histoire.php
+++ histoire.php
@@ -1,54 +1,48 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Site web - Canal du Midi</title>
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/language.css">
-    <link rel="stylesheet" href="css/slideshow.css">
-  </head>
-  <body>
-    <div id="container">
-      <?php include('includes/header.php'); ?>
 
-      <?php include('includes/menu.php'); ?>
+<head>
+  <meta charset="utf-8">
+  <title>Site web - Canal du Midi</title>
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/language.css">
+  <link rel="stylesheet" href="css/slideshow.css">
+  <!-- link rel="stylesheet" href="./includes/timeline/timeline.css"-->
 
-<div class="left">
-<div class="box_left">
-	<div class="box_title">Histoire</div>
-<h2>Pr&eacute;sentation&nbsp;</h2>
-<p style="text-align: justify;"><span style="font-weight: 400;">&nbsp; &nbsp; Le canal du midi ma</span><span>rque un</span><span>e &eacute;ta</span><span>pe cl&eacute; mettant face &agrave; face deux &eacute;poques, la Renaissance et les temps modernes. Plus grand ouvrage public depuis le d&eacute;clin de l&rsquo;Empire Romain, par sa taille, sa stabili</span><span>t&eacute; et dont l&rsquo;&eacute;dification t&eacute;moigne d&rsquo;une administration complexe mais intr&eacute;pide, les territoires l&rsquo;abritant se sont impr&eacute;gn</span><span>&eacute;s de son aura autant qu&rsquo;il a &eacute;t&eacute; accommod&eacute; suivant l&rsquo;esprit verdoyant, et fascinant des paysages alentour</span>s, tout en en imposant par sa carrure fuselant vers la mer; dont la lin&eacute;arit&eacute; n&rsquo;est pas sans refl&eacute;ter un urbanisme adopt&eacute; de certains ouvrages romains.</p>
-<p style="text-align: justify;"><img class="imageLeft" src="http://projetbabel.org/fluvial/images_rica/rica-midi-canal-jonction-lapie.jpg" alt="Photo illustrant la justesse de la lin&eacute;arit&eacute; du canal du midi (1950. Ecluse de Saint-Cyr)" width="257" height="313" /></p>
-<p style="text-align: justify;"><span style="font-weight: 400;">La profusion d&rsquo;id&eacute;es techniq</span>ues et architecturales mais aussi sociales durant la construction du canal, faisant du tout, un ensemble esth&eacute;tique et&nbsp;adapt&eacute;, manifeste qu&rsquo;il entretient une p&eacute;riode marquante de l&rsquo;histoire&nbsp;europ&eacute;enne, notamment dans la navigation, le transport fluvial alors soutenu des techniques du g&eacute;nie civil hydraulique.</p>
+</head>
 
-<h2><strong>Histoire</strong></h2>
-<!-- Ce commentaire est visible uniquement dans l'éditeur de source -->
-<p style="text-align: justify;"><span style="font-weight: 400;">C&rsquo;est Pierre-Paul Riquet qui fit construire au XVII si&egrave;cle, le canal du midi, anciennement nomm&eacute; canal royal du Languedoc. Alors que le canal a inspir&eacute;&nbsp;</span></p>
-<p style="text-align: justify;"><img class="imageRight" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Pierre-Paul_Riquet_%281604-1680%29.jpg/568px-Pierre-Paul_Riquet_%281604-1680%29.jpg" alt="Pierre-Paul Riquet" width="216" height="228" style="float: right;margin-left: 10px;" /></p>
-<p style="text-align: justify;"><span style="font-weight: 400;">&nbsp; &nbsp; </span></p>
-<p style="text-align: justify;"><span style="font-weight: 400;">de&nbsp;</span>nombreux autres ouvrages, il a lui-m&ecirc;me &eacute;t&eacute; inspir&eacute; de canaux&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; am&eacute;nag&eacute;s depuis l&rsquo;antiquit&eacute;.&nbsp;</p>
-<p style="text-align: justify;"><span style="font-weight: 400;">La construction d&rsquo;une voie navigable reliant l&rsquo;oc&eacute;an Atlantique et la mer M&eacute;diterran&eacute;e repr&eacute;sente un enjeu &eacute;cono</span>mique et politique; mais n&rsquo;est pas une id&eacute;e nouvelle car de nombreux projets pr&eacute;c&eacute;dant se sont heurt&eacute;s &agrave; des difficult&eacute;s en grande parties due &agrave; des s<span style="font-weight: 400;">oucis d&rsquo;alimentation en eau et des topographies jug&eacute;es trop compliqu&eacute;es &agrave; g&eacute;rer. En effet le canal permettait aux transports de marchandises et plus g&eacute;n&eacute;ralement les bateaux, un contournement de la p&eacute;ninsule Ib&eacute;rique et ainsi &eacute;viter la taxe espagnole au passage du d&eacute;troit de Gibraltar, tout en assurant une route directe et s&eacute;curis&eacute;e, esquivant les routes terrestres plus ou moins praticables ou les mauvaises rencontres.</span></p>
-<p style="text-align: justify;"><span style="font-weight: 400;">Alors que la r&eacute;gion du Languedoc jusqu&rsquo;&agrave; celle de Toulouse, fait preuve d&rsquo;hostilit&eacute; &agrave; l&rsquo;&eacute;gard du pouvoir royal. Le canal permet de priver l&rsquo;Espagne d&rsquo;une partie de ses ressources tout en d&eacute;veloppant le commerce du Midi.&nbsp;</span></p>
-<p style="text-align: justify;"><span style="font-weight: 400;">Ainsi, Louis XIV consid&egrave;re positivement le projet du canal de Pierre-Paul Riquet qu&rsquo;il d&eacute;couvre en 1662. En 1664, l'expertise du projet par les &Eacute;tats du Languedoc confirme sa faisabilit&eacute;, et est officiellement valid&eacute;e par le roi. Les travaux commencent en 1666. Le canal est inaugur&eacute; le 15 mai 1681 et finalement ouvert au trafic en juillet 1684 sur la totalit&eacute; de son parcours. Ainsi il se situe &agrave; cheval sur trois d&eacute;partements: la Haute-Garonne, l'Aude et l'H&eacute;rault. Entre Marseillan, o&ugrave; il d&eacute;bouche sur l'&eacute;tang de Thau, et Toulouse, il parcourt 241 km.</span></p>
-</div>
-</div>
-<div class="right">
-  <div class="box_right">
+<body>
+  <div id="container">
+    <?php include('includes/header.php'); ?>
+
+    <?php include('includes/menu.php'); ?>
+
+    <div class="left">
+      <div class="box_left">
+
+        <div class="box_title">Histoire</div>
+        <div>
+          <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1PPw-xPYOxIyyWKbhg2-iR-suQlJ2D1rqmD95vQWxC-4&font=Default&lang=fr&initial_zoom=1&width=100%&height=100%' width='100%' height='900px' frameborder='0'></iframe>
+        </div>
+
+      </div>
+    </div>
+    <div class="right">
+      <div class="box_right">
 
 
-    <div class="box_title" style="
+        <div class="box_title" style="
     background: rgb(66,66,66); /* Old browsers */
     background: -moz-linear-gradient(top, rgb(66,66,66) 0%, rgb(51,51,51) 100%); /* FF3.6-15 */
     background: -webkit-linear-gradient(top, rgb(66,66,66) 0%,rgb(51,51,51) 100%); /* Chrome10-25,Safari5.1-6 */
     background: linear-gradient(to bottom, rgb(66,66,66) 0%,rgb(51,51,51) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-    border-bottom: 3px solid rgb(38, 38, 38);"
-    >Catégorie</div>
-test
+    border-bottom: 3px solid rgb(38, 38, 38);">Catégorie</div>
+        test
+      </div>
     </div>
-</div>
 
 
-<div class="clear"></div>
+    <div class="clear"></div>
 
 
-<?php include("includes/footer.php"); ?>
+    <?php include("includes/footer.php"); ?>
