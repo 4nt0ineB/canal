e41e205fb3fc4cc35f96918f397b925082a1107c
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-05-04 12:21:38 +0200

    Language selection done

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,47 @@
+<?php
+	session_start();
+
+	if (isset($_POST["recup_language"])){
+
+		if ($_POST["recup_language"] == "en"){
+			$_SESSION["lang"] = "en";
+			$_SESSION["otherlanguages"] = ["fr","es"];
+
+		} else if ($_POST["recup_language"] == "es") {
+			$_SESSION["lang"] = "es";
+			$_SESSION["otherlanguages"] = ["fr","en"];
+
+		} else if ($_POST["recup_language"] == "fr") {
+			$_SESSION["lang"] = "fr";
+			$_SESSION["otherlanguages"] = ["en","es"];
+		}
+
+	} else if (!isset($_SESSION["lang"])){
+		$_SESSION["lang"] = "fr";
+		$_SESSION["otherlanguages"] = ["en","es"];
+	}
+
+?>
+ <form method="POST" id="choix1" > <input type="hidden" name="recup_language" value="<?php echo $_SESSION["otherlanguages"][0]; ?>"/> </form>
+ <form  method="POST" id="choix2"> <input type="hidden" name="recup_language" value="<?php echo $_SESSION["otherlanguages"][1]; ?>"/> </form>
+
 <div id="header">
     <div id="search"><input type="text" id="searchbar" name="search" minlength="4" size="10" placeholder="Recherche..."></div>
     <div id="language">
 
       <div id="lang_selector" class="language-dropdown">
-      <label for="toggle" class="lang-flag lang-fr" title="Sélectionnez votre language">
+      <label for="toggle" class="lang-flag lang-<?php echo $_SESSION["lang"];?>" title="Sélectionnez votre language">
       <span class="flag"></span>
       </label>
       <ul class="lang-list">
-      <li class="lang lang-fr selected" title="FR">
+      <li class="lang lang-<?php echo $_SESSION["otherlanguages"][0]; ?> selected" title="<?php echo $_SESSION["otherlanguages"][0]; ?>">
       <span class="flag"></span>
       </li>
-      <li class="lang lang-uk" title="EN">
-      <a href="./en"><span class="flag"></span></a>
+      <li class="lang lang-<?php echo $_SESSION["otherlanguages"][0]; ?>" title="<?php echo $_SESSION["otherlanguages"][0]; ?>">
+      <a href='#' onclick='document.getElementById("choix1").submit()'><span class="flag"></span></a>
       </li>
-      <li class="lang lang-es" title="ES">
-      <a href="./es"><span class="flag"></span></a>
+      <li class="lang lang-<?php echo $_SESSION["otherlanguages"][1]; ?>" title="<?php echo $_SESSION["otherlanguages"][1]; ?>">
+      <a href='#' onclick='document.getElementById("choix2").submit()'><span class="flag"></span></a>
       </li>
       </ul>
       </div>
--- includes/menu.php
+++ includes/menu.php
@@ -5,6 +5,7 @@
     <li><a href="quisommesnous.php">Qui sommes-nous ?</a></li> |
     <li><a href="histoire.php">Histoire</a></li> |
     <li><a href="#">Patrimoine</a></li> |
-    <li><a href="#">Parcours</a></li>
+    <li><a href="#">Parcours</a></li> 
+    <li><FONT color="#33a0ff"><?php echo $_SESSION["lang"]; ?></FONT></li>
   </ul>
 </div>
