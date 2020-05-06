4b369965a0d208d88b9dc5ae96092781ccfe0ec7
cydaw6 <cydaw6@gmail.com>
2020-05-06 22:30:35 +0200

    Traduction des images nature.php

--- nature.php
+++ nature.php
@@ -27,54 +27,66 @@
                 <p><?php echo $txt[2][$_SESSION["lang"]]; ?></p>
                 <p><?php echo $txt[4][$_SESSION["lang"]]; ?></p>
                 <div class="gallery-image">
-                    <div class="img-box">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg/450px-P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Library</p>
+                    <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg/450px-P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg/450px-P1280621_Paris_V_Jardin_des_Plantes_Micocoulier_rwk.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[7][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="img-box">
-                        <img src="https://zupimages.net/up/20/19/253n.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Night Sky</p>
+                    </a>
+                    <a href="https://zupimages.net/up/20/19/253n.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://zupimages.net/up/20/19/253n.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[8][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="img-box">
-                        <img src="https://zupimages.net/up/20/19/re9i.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Tea Talk</p>
+                    </a>
+                    <a href="https://zupimages.net/up/20/19/re9i.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://zupimages.net/up/20/19/re9i.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[9][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="img-box">
-                        <img src="https://cdn.pixabay.com/photo/2019/04/10/20/51/poplars-4118191_960_720.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Road</p>
+                    </a>
+                    <a href="https://cdn.pixabay.com/photo/2019/04/10/20/51/poplars-4118191_960_720.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://cdn.pixabay.com/photo/2019/04/10/20/51/poplars-4118191_960_720.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[10][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="img-box">
-                        <img src="https://zupimages.net/up/20/19/a981.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Sea</p>
+                    </a>
+                    <a href="https://zupimages.net/up/20/19/a981.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://zupimages.net/up/20/19/a981.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[11][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="img-box">
-                        <img src="https://zupimages.net/up/20/19/27ao.jpg" alt="" />
-                        <div class="transparent-box">
-                            <div class="caption">
-                                <p>Vintage</p>
+                    </a>
+                    <a href="https://zupimages.net/up/20/19/27ao.jpg" target="_blank">
+                        <div class="img-box">
+                            <img src="https://zupimages.net/up/20/19/27ao.jpg" alt="" />
+                            <div class="transparent-box">
+                                <div class="caption">
+                                    <p><?php echo $txt[12][$_SESSION["lang"]]; ?></p>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
 
 
@@ -89,7 +101,7 @@
 
                             <div class="transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[13][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -100,7 +112,7 @@
                             <img src="https://cdn.pixabay.com/photo/2017/06/30/03/24/carp-2457084_960_720.jpg" alt="" />
                             <div class="transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[14][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
 
@@ -112,7 +124,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/00/Brochet.jpg/800px-Brochet.jpg" alt="" />
                             <div class="transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[15][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
 
@@ -124,7 +136,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Sandre_%28Sander_lucioperca%29.jpg/800px-Sandre_%28Sander_lucioperca%29.jpg" alt="" />
                             <div class="transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[16][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
 
@@ -136,7 +148,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Egli_%28Perca_fluviatilis%29.jpg/800px-Egli_%28Perca_fluviatilis%29.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[17][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
 
@@ -148,18 +160,18 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Silurus_glanis_02.jpg/800px-Silurus_glanis_02.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[18][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
 
                         </div>
                     </a>
-                    <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Silurus_glanis_02.jpg/800px-Silurus_glanis_02.jpg" target="_blank">
+                    <a href="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Ichthyologie%3B_ou%2C_Histoire_naturelle_des_poissons_%28Plate_88%29_%287064440191%29.jpg/800px-Ichthyologie%3B_ou%2C_Histoire_naturelle_des_poissons_%28Plate_88%29_%287064440191%29.jpg" target="_blank">
                         <div class="img-box">
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Ichthyologie%3B_ou%2C_Histoire_naturelle_des_poissons_%28Plate_88%29_%287064440191%29.jpg/800px-Ichthyologie%3B_ou%2C_Histoire_naturelle_des_poissons_%28Plate_88%29_%287064440191%29.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[19][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -169,7 +181,7 @@
                             <img src="https://cdn.pixabay.com/photo/2014/12/11/07/49/eel-563941_960_720.jpg" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[20][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -179,7 +191,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Lamproie_Planer57.jpg/1200px-Lamproie_Planer57.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[21][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -189,7 +201,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Alosa_alosa.jpg/800px-Alosa_alosa.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[22][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -199,7 +211,7 @@
                             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Ragondin_%28Myocastor_coypus%29_%2839%29.jpg/800px-Ragondin_%28Myocastor_coypus%29_%2839%29.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[23][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -209,7 +221,7 @@
                             <img src="https://live.staticflickr.com/3104/3207317876_7032776277_b.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[24][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
@@ -219,17 +231,17 @@
                             <img src="https://live.staticflickr.com/5511/10577707366_496f4bde76_b.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[25][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
                     </a>
-                    <a href="https://lh3.googleusercontent.com/proxy/yGxqJ8W2_7zpz_7dit7MuIs2ZIuOmQqEqrXk5daosian4PnUn2s_fCtXZ9vYc_AX1RL0wSz3SGdZQuahE1PrV8i3xn7mjhSi0vYQa29BxaSm_14" target="_blank">
+                    <a href="https://p0.piqsels.com/preview/799/191/811/common-moorhen-grunfu%C3%9Figes-pond-chicken-moorhen-chicken.jpg" target="_blank">
                         <div class="img-box">
-                            <img src="https://lh3.googleusercontent.com/proxy/yGxqJ8W2_7zpz_7dit7MuIs2ZIuOmQqEqrXk5daosian4PnUn2s_fCtXZ9vYc_AX1RL0wSz3SGdZQuahE1PrV8i3xn7mjhSi0vYQa29BxaSm_14" alt="" />
+                            <img src="https://p0.piqsels.com/preview/799/191/811/common-moorhen-grunfu%C3%9Figes-pond-chicken-moorhen-chicken.jpg" alt="" />
                             <div class=" transparent-box">
                                 <div class="caption">
-                                    <p>Vintage</p>
+                                    <p><?php echo $txt[26][$_SESSION["lang"]]; ?></p>
                                 </div>
                             </div>
                         </div>
