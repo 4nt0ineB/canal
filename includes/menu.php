5b989fbb2685304e84a34fedce63873548ae740a
cydaw6 <cydaw6@gmail.com>
2020-05-08 22:40:33 +0200

    ++

--- includes/menu.php
+++ includes/menu.php
@@ -8,7 +8,7 @@ $nrow = ($_SESSION["lang"] == "fr") ? 0 : 1; // c'est une table chelou du coup f
   <ul>
     <li><a href="index.php"><?php echo $txtNav[$nrow]["t1"]; ?></a></li> |
     <li><a href="histoire.php"><?php echo $txtNav[$nrow]["t4"]; ?></a></li> |
-    <li><a href="naviguer.php">Naviguer</a></li> |
+    <li><a href="naviguer.php"><?php echo $txtNav[$nrow]["t8"]; ?></a></li> |
     <li><a href="nature.php"><?php echo $txtNav[$nrow]["t7"]; ?></a></li> |
     <li class="rubrique-drop"><a href="javascript:void(0)"><?php echo $txtNav[$nrow]["t6"]; ?></a>
       <div class="dropdown-content">
