354732019e202355e62dc3251666ec39809a145e
Zkimi <20417186+zkimi@users.noreply.github.com>
2020-05-19 16:03:06 +0200

    admin panel change btn style

--- admin/includes/aside.php
+++ admin/includes/aside.php
@@ -2,12 +2,12 @@
   <div class="box_right">
     <div class="box_title black">Navigation administrateur</div>
     <center>
-      <div class="button-survey" style="width:200px;"><a href="./index.php" style="text-decoration: none;color:inherit;">Statistiques</a></div>
-      <div class="button-survey" style="width:200px;"><a href="./new_article.php" style="text-decoration: none;color:inherit;">Créer un article</a></div>
-      <div class="button-survey" style="width:200px;"><a href="./articles.php" style="text-decoration: none;color:inherit;">Voir les articles</a></div>
-      <div class="button-survey" style="width:200px;"><a href="./new_fiche.php" style="text-decoration: none;color:inherit;">Créer une fiche</a></div>
-      <div class="button-survey" style="width:200px;"><a href="./fiches.php" style="text-decoration: none;color:inherit;">Voir les fiches</a></div>
-      <div class="button-survey" style="width:200px;"><a href="./visiteurs.php" style="text-decoration: none;color:inherit;">Voir les visiteurs</a></div>
+      <div class="button-survey gradientop" style="width:200px;"><a href="./index.php" style="text-decoration: none;color:inherit;">Statistiques</a></div>
+      <div class="button-survey gradientop" style="width:200px;"><a href="./visiteurs.php" style="text-decoration: none;color:inherit;">Voir les visiteurs</a></div>
+      <div class="button-survey gradientbg" style="width:200px;"><a href="./new_article.php" style="text-decoration: none;color:inherit;">Créer un article</a></div>
+      <div class="button-survey gradientbg" style="width:200px;"><a href="./articles.php" style="text-decoration: none;color:inherit;">Voir les articles</a></div>
+      <div class="button-survey gradientoy" style="width:200px;"><a href="./new_fiche.php" style="text-decoration: none;color:inherit;">Créer une fiche</a></div>
+      <div class="button-survey gradientoy" style="width:200px;"><a href="./fiches.php" style="text-decoration: none;color:inherit;">Voir les fiches</a></div>
     </center>
   </div>
 </div>
