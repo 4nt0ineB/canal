6c89c386b1cc305ff46844dc4f176164e639d08c
cydaw6 <cydaw6@gmail.com>
2020-06-14 13:29:23 +0200

    c'était pas le dernier commit en fait

--- histoire.php
+++ histoire.php
@@ -12,14 +12,14 @@
 
 <body>
   <div id="container">
-    <?php 
+    <?php
     include('includes/header.php');
 
     // statistiques
     $query_count = $db->query("SELECT compteur FROM stats WHERE page=\"histoire\";")->fetch();
     $count = $query_count["compteur"];
     $db->query("UPDATE stats SET compteur=$count+1 WHERE page=\"histoire\";");
-     ?>
+    ?>
 
     <?php include('includes/menu.php'); ?>
 
@@ -37,92 +37,99 @@
           ?>
             <div class="box_title">Histoire</div>
             <div>
-              <iframe src=https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1PPw-xPYOxIyyWKbhg2-iR-suQlJ2D1rqmD95vQWxC-4&font=Default&lang=fr&initial_zoom=1&width=100%&height=100%' width='100%' height='900px' frameborder='0'></iframe>
+              <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1PPw-xPYOxIyyWKbhg2-iR-suQlJ2D1rqmD95vQWxC-4&font=Default&lang=fr&initial_zoom=1&width=100%&height=100%' width='100%' height='900px' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
             <?php
-          }
+          } elseif ($_SESSION['lang'] == "es") {
             ?>
+              <div class="box_title">Historia</div>
+              <div>
+                <iframe src='https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1O9fKankY8C0d6N8fXH4a6InWiwYi_i7NodHkFTRVg-w&font=Default&lang=es&initial_zoom=2&height=100%' width='100%' height='900px' webkitallowfullscreen mozallowfullscreen allowfullscreen frameborder='0'></iframe>
+
+              <?php
+            }
+              ?>
+              </div>
+
+            </div>
+          </div>
+          <div class="right">
+            <div class="box_right">
+
+
+              <div class="box_title black">
+                <?php
+                if ($_SESSION["lang"] == "en") {
+                  echo "Want to know more?";
+                } elseif ($_SESSION["lang"] == "fr") {
+                  echo "Vous voulez en savoir plus ?";
+                }
+                ?>
+              </div>
+              <center><br>
+                <?php
+                if ($_SESSION["lang"] == "en") {
+                  echo "<a href=\"https://fr.wikipedia.org/wiki/Canal_du_Midi\" target=\"_blank\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png\" style=\"width:90px;\"></a><br>
+              More details on the Wikipedia page labeled <b>\"quality article\"</b> !";
+                } elseif ($_SESSION["lang"] == "fr") {
+                  echo "<a href=\"https://fr.wikipedia.org/wiki/Canal_du_Midi\" target=\"_blank\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png\" style=\"width:90px;\"></a><br>
+              Plus de détails sur la page Wikipédia classée <b>\"article de qualité\"</b> !";
+                }
+                ?>
+              </center>
             </div>
 
+            <br>
+
+            <div class="box_right">
+              <div class="box_title black"><?php
+                                            if ($_SESSION["lang"] == "fr") {
+                                              echo "Une image de notre galerie";
+                                            } else if ($_SESSION["lang"] == "en") {
+                                              echo "An image from our gallery";
+                                            } else if ($_SESSION["lang"] == "es") {
+                                              echo "Una imagen de nuestra galería";
+                                            }
+                                            ?></div>
+
+
+              <center>
+                <iframe src="includes/iframe_galerie.php" scrolling="no" frameborder="0"></iframe>
+                <hr>
+                <a href="galerie.php">
+                  <?php
+                  if ($_SESSION["lang"] == "fr") {
+                    echo "Voir notre galerie";
+                  } else if ($_SESSION["lang"] == "en") {
+                    echo "See our gallery";
+                  } else if ($_SESSION["lang"] == "es") {
+                    echo "Vea nuestra galería";
+                  }
+                  ?></a>
+              </center>
+            </div>
+
+            <br>
+
+            <div class="box_right">
+              <div class="box_title black"><?php
+                                            if ($_SESSION["lang"] == "fr") {
+                                              echo "Nos réseaux sociaux";
+                                            } else if ($_SESSION["lang"] == "en") {
+                                              echo "Our social medias";
+                                            } else if ($_SESSION["lang"] == "es") {
+                                              echo "Nuestras redes sociales";
+                                            }
+                                            ?></div>
+
+              <center style="margin-top:5px;">
+                <a href="https://www.facebook.com/Canal-du-Midi-M%C3%A9diation-Culturelle-et-Num%C3%A9rique-106766767743771" target="_blank" class="socialmedia"><i class="devicon-facebook-plain" style="font-size:34px;"></i></a>
+                <a href="https://twitter.com/DuCulturelle" target="_blank" class="socialmedia"><i class="devicon-twitter-plain" style="font-size:30px;"></i></a>
+              </center>
+            </div>
           </div>
-      </div>
-      <div class="right">
-        <div class="box_right">
 
 
-          <div class="box_title black">
-            <?php
-            if ($_SESSION["lang"] == "en") {
-              echo "Want to know more?";
-            } elseif ($_SESSION["lang"] == "fr") {
-              echo "Vous voulez en savoir plus ?";
-            }
-            ?>
-          </div>
-          <center><br>
-            <?php
-            if ($_SESSION["lang"] == "en") {
-              echo "<a href=\"https://fr.wikipedia.org/wiki/Canal_du_Midi\" target=\"_blank\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png\" style=\"width:90px;\"></a><br>
-              More details on the Wikipedia page labeled <b>\"quality article\"</b> !";
-            } elseif ($_SESSION["lang"] == "fr") {
-              echo "<a href=\"https://fr.wikipedia.org/wiki/Canal_du_Midi\" target=\"_blank\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/220px-Wikipedia-logo-v2.svg.png\" style=\"width:90px;\"></a><br>
-              Plus de détails sur la page Wikipédia classée <b>\"article de qualité\"</b> !";
-            }
-            ?>
-          </center>
-        </div>
-
-        <br>
-
-              <div class="box_right">
-        <div class="box_title black"><?php 
-        if ($_SESSION["lang"] == "fr"){
-          echo "Une image de notre galerie";
-         } else if ($_SESSION["lang"] == "en"){
-          echo "An image from our gallery";
-         } else if ($_SESSION["lang"] == "es"){
-          echo "Una imagen de nuestra galería";
-         }
-         ?></div>
-      
-
-        <center>
-          <iframe src="includes/iframe_galerie.php" scrolling="no" frameborder="0" ></iframe>
-          <hr>
-          <a href="galerie.php">
-          <?php 
-          if ($_SESSION["lang"] == "fr"){
-            echo "Voir notre galerie";
-           } else if ($_SESSION["lang"] == "en"){
-            echo "See our gallery";
-           } else if ($_SESSION["lang"] == "es"){
-            echo "Vea nuestra galería";
-           }
-           ?></a>
-        </center>
-      </div>
-      
-      <br>
-
-<div class="box_right">
-    <div class="box_title black"><?php
-                                  if ($_SESSION["lang"] == "fr") {
-                                    echo "Nos réseaux sociaux";
-                                  } else if ($_SESSION["lang"] == "en") {
-                                    echo "Our social medias";
-                                  } else if ($_SESSION["lang"] == "es") {
-                                    echo "Nuestras redes sociales";
-                                  }
-                                  ?></div>
-
-    <center style="margin-top:5px;">
-    <a href="https://www.facebook.com/Canal-du-Midi-M%C3%A9diation-Culturelle-et-Num%C3%A9rique-106766767743771" target="_blank" class="socialmedia"><i class="devicon-facebook-plain" style="font-size:34px;"></i></a>
-    <a href="https://twitter.com/DuCulturelle" target="_blank" class="socialmedia"><i class="devicon-twitter-plain" style="font-size:30px;"></i></a>
-    </center>
-  </div>
-</div>
+          <div class="clear"></div>
 
 
-      <div class="clear"></div>
-
-
-      <?php include("includes/footer.php"); ?>
+          <?php include("includes/footer.php"); ?>
